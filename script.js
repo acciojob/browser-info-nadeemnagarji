58e191bc0af8da4335dd9a1ee0c50936ee0a3d20
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-16 22:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const div = document.createElement('div')
+  div.id = 'browser-info'
+div.innerHTML = `You are using ${navigator.appName} ${navigator.appVersion}version`
+document.body.appendChild(div)
